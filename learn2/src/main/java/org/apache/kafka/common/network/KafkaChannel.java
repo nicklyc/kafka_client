1035c6fe9b567be8352c66022b02b04a0aa91722
maochao <29491336@qq.com>
2019-07-03 23:17:04 +0800

    Comment:selector 源码注释

--- learn2/src/main/java/org/apache/kafka/common/network/KafkaChannel.java
+++ learn2/src/main/java/org/apache/kafka/common/network/KafkaChannel.java
@@ -288,6 +288,7 @@ public class KafkaChannel {
         if (this.send != null)
             throw new IllegalStateException("Attempt to begin a send operation with prior send operation still in progress, connection id is " + id);
         this.send = send;
+        //添加到interestOps
         this.transportLayer.addInterestOps(SelectionKey.OP_WRITE);
     }
 
